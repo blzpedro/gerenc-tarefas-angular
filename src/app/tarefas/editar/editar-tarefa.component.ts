81e7dab41e6f2e894cd9b6240330e28db249dab1
Pedro Melo <phenriqmelo99@gmail.com>
2019-11-06 13:06:58 -0300

    projeto gerenciador de tarefas

---
+++ src/app/tarefas/editar/editar-tarefa.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Tarefa, TarefaService } from '../shared';
+import { NgForm } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-editar-tarefa',
+  templateUrl: './editar-tarefa.component.html',
+  styleUrls: ['./editar-tarefa.component.css']
+})
+
+export class EditarTarefaComponent implements OnInit {
+  @ViewChild('formTarefa', { static: true }) formTarefa: NgForm;
+  tarefa: Tarefa;
+
+  constructor(private tarefaService: TarefaService, private route: ActivatedRoute, private router: Router) { }
+
+  ngOnInit() {
+    const id = +this.route.snapshot.params['id'];
+    this.tarefa = this.tarefaService.buscarporID(id);
+  }
+
+  atualizar(): void{
+    if (this.formTarefa.form.valid){
+      this.tarefaService.atualizar(this.tarefa);
+      this.router.navigate(['/tarefas']);
+    }
+  }
+
+}
